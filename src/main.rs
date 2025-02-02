2b6a3731a3b2ac7440422771575ad1232b99eaee
ritonun <ritonun@fakeemail.com>
2025-02-02 17:20:18 +0100

    refactor to use nice Video structure

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use serde_json::Value;
+use std::fs;
 use std::path::PathBuf;
-use std::process::Command as ProcessCommand;
+use std::process::Command;
 
 struct Config {
     output_path: PathBuf,
@@ -8,9 +9,15 @@ struct Config {
     delete_local: bool,
 }
 
+struct Video {
+    path: PathBuf,
+    name: String,
+    url: String,
+}
+
 fn get_playlist_videos(config: &Config) -> Result<Value, std::io::Error> {
     println!("Requesting playlist {}", config.url);
-    let output = ProcessCommand::new("yt-dlp")
+    let output = Command::new("yt-dlp")
         .args(["--flat-playlist", "-J", &config.url]) // JSON output
         .output()?;
 
@@ -18,6 +25,66 @@ fn get_playlist_videos(config: &Config) -> Result<Value, std::io::Error> {
     let json_value: Value = serde_json::from_str(&output)?;
 
     Ok(json_value)
+}
+
+fn get_online_videos(config: &Config) -> Vec<Video> {
+    // make the request and get a json result of all the videos in the playlist
+    let json = match get_playlist_videos(&config) {
+        Ok(json) => json,
+        Err(e) => {
+            panic!("Error fetching playlist: {}", e);
+        }
+    };
+
+    let mut online_videos: Vec<Video> = Vec::new();
+
+    if let Some(entries) = json["entries"].as_array() {
+        for video in entries {
+            let mut v = Video {
+                url: String::new(),
+                name: String::new(),
+                path: PathBuf::new(),
+            };
+            if let Some(title) = video["title"].as_str() {
+                v.name = title.to_string();
+            }
+            if let Some(url) = video["url"].as_str() {
+                v.url = url.to_string();
+            }
+
+            if !v.url.is_empty() && !v.name.is_empty() {
+                online_videos.push(v);
+            }
+        }
+    }
+
+    online_videos
+}
+
+fn get_local_videos(config: &Config) -> Vec<Video> {
+    let mut videos: Vec<Video> = Vec::new();
+
+    // get all files in the directory
+
+    match fs::read_dir(&config.output_path) {
+        Ok(entries) => {
+            for entry in entries {
+                let entry = entry.unwrap();
+                if entry.path().is_file() {
+                    // check it is a file
+                    let video = Video {
+                        path: entry.path(),
+                        name: entry.file_name().into_string().unwrap(),
+                        url: String::new(),
+                    };
+                    videos.push(video);
+                }
+            }
+        }
+        Err(e) => panic!("Error reading directory: {}", e),
+    }
+
+    videos
 }
 
 fn main() {
@@ -48,16 +115,15 @@ fn main() {
     // match user flags with config param
     let config = Config {
         url: matches.get_one::<String>("url").unwrap().to_string(),
-        output_path: matches.get_one::<PathBuf>("output_path").unwrap().clone(),
+        output_path: PathBuf::from(matches.get_one::<String>("output_path").unwrap()),
         delete_local: matches.get_flag("delete_local"),
     };
 
-    // make the request and get a json result of all the videos in the playlist
-    let json = match get_playlist_videos(&config) {
-        Ok(json) => json,
-        Err(e) => {
-            eprintln!("Error fetching playlist: {}", e);
-            return;
-        }
-    };
+    // create a vector containning all local video
+    let local_videos = get_local_videos(&config);
+
+    // create vector containning all online video
+    let online_videos = get_online_videos(&config);
+
+    println!("{} {}", local_videos.len(), online_videos.len());
 }
